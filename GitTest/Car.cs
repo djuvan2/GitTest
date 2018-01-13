e7135770e0df59b212797a89f25ca9a531906704
darko.juvan <darko.juvan@gmail.com>
2018-01-13 08:50:33 -0800

    Added Car class

---
+++ GitTest/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Car
+    {
+        int wheels = 0;
+
+        public Car()
+        {
+            wheels = 4;
+        }
+    }
+}
