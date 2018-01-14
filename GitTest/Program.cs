7919e1df4f8d4bfe0dfc3d4b0e6f1745df1b3e83
darko.juvan <darko.juvan@gmail.com>
2018-01-13 17:28:48 -0800

    master change 11

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -10,7 +10,7 @@ namespace GitTest
     // branch5 change 1
     class Program
     {
-        static int x = 1;   //master change10
+        static int x = 1;   //master change 11
 
         static void Main(string[] args)
         {
