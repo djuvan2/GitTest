741dba62c05f3e735e0921b8ab25927f44720b6c
darko.juvan <darko.juvan@gmail.com>
2018-01-13 18:41:34 -0800

    branch5 change 10

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 // branch6 change1
 namespace GitTest
 {
-    // branch5 change 9
+    // branch5 change 10
     class Program
     {
         static int x = 1;   //master change 12
